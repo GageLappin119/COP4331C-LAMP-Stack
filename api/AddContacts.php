1929842b926dc1a5ac32f810f89f61e8ac8601dd
GamerNarhwal <68747773+GamerNarhwal@users.noreply.github.com>
2025-09-02 14:05:45 -0400

    Updated AddContact
    Now ensures that you are not adding a contact with the same first and last name and aligns with the naming scheme of the database

--- api/AddContacts.php
+++ api/AddContacts.php
@@ -2,17 +2,46 @@
 	require_once 'db.php';
 	
 	$inData = getRequestInfo();
+
+	$userID = $inData["UserID"];
 	$firstName = $inData["FirstName"];
 	$lastName = $inData["LastName"];
-	$phoneNumber = $inData["PhoneNumber"];
-	$emailAddress = $inData["EmailAddress"];
-	$contactID = $inData["ContactID"];
+	$phoneNumber = $inData["Phone"];
+	$emailAddress = $inData["Email"];
 
-	$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, PhoneNumber, EmailAddress, ContactID) VALUES (?,?,?,?,?)");
-	$stmt -> bind_param("ssssi" , $firstName, $lastName, $phoneNumber, $emailAddress, $contactID);
+	$stmt = $conn->prepare("SELECT UserID FROM Contacts WHERE UserID = ? AND FirstName = ? AND LastName= ?");
+
+ 
+	$stmt->bind_param("iss", $userID, $firstName, $lastName);
+
 	$stmt->execute();
+
+	$result = $stmt->get_result();
+
+	if ($result->num_rows > 0)
+	{
+		returnWithError("Contact already exists");
+		$stmt->close();
+		$conn->close();
+		exit();
+	}
+
+	$stmt->close();
+
+	$stmt = $conn->prepare("INSERT INTO Contacts (UserID, FirstName, LastName, Phone, Email) VALUES (?,?,?,?,?)");
+	$stmt -> bind_param("issss" , $userID, $firstName, $lastName, $phoneNumber, $emailAddress);
+
+	$stmt->execute();
+
+	if($stmt->affected_rows > 0)
+	{
+		returnWithError("Successfully added a contact");
+	}
+	else
+	{
+		returnWithError("Failed to add a contact");
+	}
+
 	$stmt->close();
 	$conn->close();
-	returnWithError("");
-
 ?>
