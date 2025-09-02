f2eedfa8343977e281d6775aec203841e79e16e4
GamerNarhwal <68747773+GamerNarhwal@users.noreply.github.com>
2025-09-02 11:34:32 -0400

    Created Login API
    Created basic functionality for the Login API

--- api/Login.php
+++ api/Login.php
@@ -7,9 +7,55 @@
 
 	$inData = getRequestInfo();
 
+	$login = $inData["userName"];
+	$password = $inData["password"];
 
-	if (isset($stmt)) {
+	$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login = ?");
+	$stmt->bind_param("s", $login);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if ($result->num_rows == 0){
+		returnWithError("Username does not exist");
 		$stmt->close();
+		$conn->close();
+		exit();
 	}
+
+	$stmt->close();
+
+	$stmt = $conn->prepare("SELECT ID, FirstName, LastName, Password FROM Users WHERE Login = ?");
+	$stmt->bind_param("s", $login);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if ($row = $result->fetch_assoc())
+	{
+		if (password_verify($password, $row["Password"])){
+			$responseData = [
+				"id" => $row["ID"],
+				"FirstName" => $row["FirstName"],
+				"LastName" => $row["LastName"]
+			];
+
+			sendResultInfoAsJson(json_encode($responseData));
+		}
+		else
+		{
+			returnWithError("Incorrect password");
+			$stmt->close();
+			$conn->close();
+			exit();
+		}
+	}
+	else
+	{
+		returnWithError("User not found");
+		$stmt->close();
+		$conn->close();
+		exit();
+	}
+
+	$stmt->close();	
 	$conn->close();
 ?>
