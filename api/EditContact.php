90bc5035c733d402f73e2e460131568c5b5a1dfe
BraySmith <smithbrayden319@gmail.com>
2025-09-02 12:55:25 -0400

    Sample EditContact code

--- api/EditContact.php
+++ api/EditContact.php
@@ -1,15 +1,19 @@
 <?php
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-
 	require_once 'db.php';
-
+	
 	$inData = getRequestInfo();
 
+	$contactID = $inData["ContactID"];
+	$emailAddress = $inData["EmailAddress"];
+	$firstName = $inData["FirstName"];
+	$lastName = $inData["LastName"];
+	$phoneNumber = $inData["PhoneNumber];
 
-	if (isset($stmt)) {
-		$stmt->close();
-	}
+	$stmt = $conn->prepare("UPDATE Contacts Set FirstName = ?, LastName = ?, PhoneNumber = ?, EmailAddress = ? WHERE ID = ?);
+	$stmt->bind_param("ssssi", $firstName, $lastName, $phoneNumber, $emailAddress, $contactID);
+	$stmt->execute();
+	$stmt->close();
 	$conn->close();
+	returnWithError("");
+
 ?>
