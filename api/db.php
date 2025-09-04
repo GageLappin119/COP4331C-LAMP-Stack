041bad6c055b4ad5131f7e74ea3b92488ac2f97d
GamerNarhwal <68747773+GamerNarhwal@users.noreply.github.com>
2025-09-04 12:09:50 -0400

    Swaggerhub testing

--- api/db.php
+++ api/db.php
@@ -1,4 +1,12 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        exit(0);
+    }
+
 	$db_host = "localhost";
 	$db_user = "ContactUser";
 	$db_pass = "DontBl1nk@ng3lsRn3@r";
